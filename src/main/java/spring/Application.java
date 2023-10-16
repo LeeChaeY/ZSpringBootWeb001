a05384113756fbf95ddf0d4f35742cde87aaee10
LeeChaeY <chland23@gmail.com>
2023-10-16 16:50:31 +0900

    create 23.10.16 16:51 master

---
+++ src/main/java/spring/Application.java
@@ -0,0 +1,14 @@
+package spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableAspectJAutoProxy
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
